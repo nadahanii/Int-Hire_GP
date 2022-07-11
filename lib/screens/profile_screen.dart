4327b97006a88db5842c3e978134d8ed80423e55
nadahanii <66536961+nadahanii@users.noreply.github.com>
2022-07-11 18:31:20 +0200

    added theme to profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
-  static const routeName = '/GetUserInfo';
+  final ThemeData profileTheme;
+  const ProfileScreen({Key? key,required this.profileTheme}) : super(key: key);
+  static const routeName = '/profile_Screen';
+  //static const routeName = '/GetUserInfo';
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
+      backgroundColor: this.profileTheme.backgroundColor,
       appBar: AppBar(
         title: Text("Profile"),
       ),
