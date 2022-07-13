3d1969028ed3cfb060f4832d1435142d5da8bd26
magdausama <74511177+magdausama@users.noreply.github.com>
2022-07-12 18:22:10 -0700

    new registeration

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,20 +1,15 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
-
-import '../helpers/components.dart';
-import '../providers/auth.dart';
 
 class LoginScreen extends StatefulWidget {
-  LoginScreen({Key? key}) : super(key: key);
+
+  //final ThemeData loginTheme;
+  LoginScreen({Key? key,/* required this.loginTheme*/}) : super(key: key);
   static const routeName = '/Login_Screen';
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
-
 class _LoginScreenState extends State<LoginScreen> {
   static const routeName = '/login';
   final Color facebookColor = const Color(0xff39579A);
@@ -53,187 +48,165 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
     );
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Theme.of(context).backgroundColor,
+      //appBar: AppBar(),
         body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Form(
-              key: _form,
-              child: SingleChildScrollView(
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Text('LOGIN',
-                        style: GoogleFonts.sourceCodePro(
-                            fontSize: 35,
-                            fontWeight: FontWeight.w800,
-                            color: Color.fromRGBO(4, 88, 125, 4))),
-                    SizedBox(
-                      height: 30.0,
-                    ),
-
-                    TextFormField(
-                      controller: emailController,
-                      decoration: InputDecoration(
-                        labelText: 'Email Address',
-                        prefixIcon: Icon(
-                          Icons.email,
-                        ),
+            padding: const EdgeInsets.all(20.0),
+            child: Form (
+            key: _form,
+               child: SingleChildScrollView(
+                 child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                              height: 20.0, ),
+                      Text(
+                        'LOGIN',
+                        style: GoogleFonts.sourceCodePro(fontSize:35 , fontWeight: FontWeight.w800, color: Color.fromRGBO(4, 88, 125,4))
+                      ),
+                      SizedBox(
+                        height: 30.0,
                       ),
 
-                      //style: widget.loginTheme.textTheme.labelMedium,
-                      keyboardType: TextInputType.emailAddress,
-                      onFieldSubmitted: (value) {
-                        //Validator
-                      },
-                      validator: (value) {
-                        if (value!.isEmpty ||
-                            !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                                .hasMatch(value)) {
-                          return 'Enter a valid email!';
-                        }
-                        return null;
-                      },
-                    ), //mail
-                    SizedBox(
-                      height: 10.0,
-                    ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Email Address',
 
-                    TextFormField(
-                      controller: passwordController,
-                      obscureText: _isObscure,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        suffixIcon: IconButton(
-                            icon: Icon(
-                              _isObscure
-                                  ? Icons.visibility
-                                  : Icons.visibility_off,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                _isObscure = !_isObscure;
-                              });
-                            }),
-                        prefixIcon: Icon(
-                          Icons.lock,
-                        ),
-                      ),
-                      //style: widget.loginTheme.textTheme.labelMedium,
-                      keyboardType: TextInputType.visiblePassword,
-                      onFieldSubmitted: (password) {
-                        //Validator
-                      },
-                      validator: (password) {
-                        if (password!.isEmpty ||
-                            !RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$')
-                                .hasMatch(password)) {
-                          return 'Enter a valid Password!';
-                        }
-                        return null;
-                      },
-                    ), //pass
-
-                    SizedBox(
-                      height: 10.0,
-                    ),
-
-                    TextButton.icon(
-                      onPressed: (() {
-                        if (_form.currentState!.validate()) {
-                          Provider.of<Auth>(context, listen: false)
-                              .login(json.encode({
-                            "email": emailController.text,
-                            "password": passwordController.text,
-                          }))
-                              .then((value) {
-                            if (value != 'login successfully') {
-                              showToast(text: value, state: ToastStates.ERROR);
-                            } else {
-                              showToast(
-                                  text: value, state: ToastStates.SUCCESS);
-                            }
-                          });
-                        }
-                      }),
-                      icon: const Icon(
-                        Icons.login,
-                        size: 28,
-                      ),
-                      label: Container(
-                        alignment: Alignment.center,
-                        width: 150,
-                        height: 35,
-                        child: const Text(
-                          'Login',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.white,
+                          prefixIcon: Icon (
+                            Icons.email,
                           ),
                         ),
-                        decoration: BoxDecoration(
-                          color: Color.fromRGBO(4, 88, 125, 1),
-                          borderRadius: BorderRadius.circular(25),
+
+                        //style: widget.loginTheme.textTheme.labelMedium,
+                        keyboardType: TextInputType.emailAddress,
+                        onFieldSubmitted: (value) {
+                          //Validator
+                        },
+                        validator: (value) {
+                          if ( value!.isEmpty ||
+                              !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                  .hasMatch(value)) {
+                            return 'Enter a valid email!';
+                          }
+                          return null;
+                        },
+                      ),   //mail
+                      SizedBox(
+                        height: 10.0,
+                      ),
+
+
+                      TextFormField(
+                        controller: passwordController,
+                        decoration: InputDecoration(labelText: 'Password',
+                          suffixIcon: IconButton(
+                              icon: Icon(
+                                _isObscure ? Icons.visibility : Icons.visibility_off,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _isObscure = !_isObscure;
+                                })
+                                ; }
+                          ),
+                          prefixIcon: Icon (
+                            Icons.lock,
+                          ),
+                        ),
+                        //style: widget.loginTheme.textTheme.labelMedium,
+                        keyboardType: TextInputType.visiblePassword,
+                        onFieldSubmitted: (password) {
+                          //Validator
+                        },
+                        validator: (password) {
+                          if ( password!.isEmpty ||
+                              !RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$')
+                                  .hasMatch(password)) {
+                            return 'Enter a valid Password!'
+                            ;
+                          }
+                          return null;
+                        },
+                      ),   //pass
+
+                      SizedBox(
+                        height: 10.0,
+                      ),
+
+                      TextButton.icon(
+                        onPressed: (() {
+                          if(_form.currentState!.validate())
+                          {
+                            Navigator.pushNamed(context, '/profile_Screen');
+                          }
+
+                        }),
+                        icon: const Icon(
+                          Icons.login,
+                          size: 28,
+                        ),
+                        label: Container(
+                          alignment: Alignment.center,
+                          width: 150,
+                          height: 35,
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                            color: Color.fromRGBO(4, 88, 125,1),
+                            borderRadius: BorderRadius.circular(25),
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 5.0,
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context)
-                            .pushReplacementNamed('/ForgetPassword_Screen');
-                      },
-                      child: Text(
-                        'Forgot My Password',
-                        style: Theme.of(context).textTheme.labelMedium,
+                      SizedBox(
+                        height: 5.0,
                       ),
-                    ),
-
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          socialButtonRect('Facebook',
-                              Color.fromRGBO(4, 88, 125, 1), Icons.facebook),
-                          SizedBox(height: 10.0),
-                          socialButtonRect('Google',
-                              Color.fromRGBO(4, 88, 125, 1), Icons.android),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Don\'t have an account?',
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pushReplacementNamed('/ForgetPassword_Screen');
+                        },
+                        child: Text(
+                          'Forgot My Password',
                           style: Theme.of(context).textTheme.labelMedium,
                         ),
-                        SizedBox(
-                          height: 5.0,
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            socialButtonRect('Facebook', Color.fromRGBO(4, 88, 125,1), Icons.facebook),
+                            SizedBox( height:10.0),
+                            socialButtonRect('Google', Color.fromRGBO(4, 88, 125,1), Icons.android),
+                          ],
                         ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context)
-                                .pushReplacementNamed('/Register_Screen');
-                          },
-                          child: Text(
-                            'Register Now',
-                            style: Theme.of(context).textTheme.bodyText2,
+                      ),
+                     SizedBox(height: 20.0,),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Don\'t have an account?',
+                            style: Theme.of(context).textTheme.labelMedium,
                           ),
-                        ),
+                          SizedBox(height: 5.0,),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pushReplacementNamed('/Register_Screen');
+                            },
+                            child: Text(
+                              'Register Now',
+                              style: Theme.of(context).textTheme.bodyText2,
+                            ),
+                          ),
                         /*  TextButton.icon(
                             onPressed: (() {
                               Navigator.of(context).pushReplacementNamed('/Register_Screen');
@@ -259,7 +232,7 @@ class _LoginScreenState extends State<LoginScreen> {
                               ),
                             ),
                           ),*/
-                        /* TextButton.icon(
+                         /* TextButton.icon(
                             onPressed: (() {
                               Navigator.pushNamed(context, '/ForgetPassword_Screen');
 
@@ -285,9 +258,14 @@ class _LoginScreenState extends State<LoginScreen> {
                               ),
                             ),
                           ),*/
-                      ],
-                    ),
-                  ]))),
-        ));
+                        ],
+                      ),
+                    ]
+            )
+        )
+    ),
+    )
+    );
   }
+
 }
