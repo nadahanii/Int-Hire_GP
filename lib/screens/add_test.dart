a5ce0e317f211087f800ad6b4202984c6268c1f7
Nada Hani <66536961+nadahanii@users.noreply.github.com>
2022-07-15 16:31:43 +0200

    Update add_test.dart

--- lib/screens/add_test.dart
+++ lib/screens/add_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../models/PersonalityDataClass.dart';
 import '../models/testClass.dart';
 import '../models/test.dart';
 import 'applicant_result_screen.dart';
@@ -13,6 +14,16 @@ class AddTest extends StatefulWidget {
 }
 
 class _AddTestState extends State<AddTest> {
+  // dumy obj
+  late PersonalityData personalityData = PersonalityData(
+      "Architect",
+      "Imaginative and strategic thinkers, with a plan for everything.",
+      "WHO IS AN ARCHITECT (INTJ)?",
+      "Architect (INTJ) Strengths",
+      "Professional know-how is often where Architects (INTJs) shine most brilliantly.",
+      "What Architects (INTJs) want",
+      "An Architect (INTJ) is a person with the Introverted.");
+  //**************************************************************
   late List<TestClass> _testData1 = Test().testData;
   List<int> _answers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   late String _testType = "";
@@ -48,78 +59,82 @@ class _AddTestState extends State<AddTest> {
         title: Text("Personality Test"),
         centerTitle: true,
       ),
-      body: Container(
-          child: SingleChildScrollView(
-        child: Column(
-          children: [
-            ListView.builder(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: _testData1.length,
-              itemBuilder: (_, i) {
-                return _radioButtonGroup(
-                  text: _testData1[i].getQuestion(),
-                  list: _testData1[i].getAnswerList().map((pair) {
-                    return ListTile(
-                      title: Text(pair.item1),
-                      leading: Radio<int>(
-                        value: pair.item2,
-                        groupValue: _answers[i],
-                        onChanged: (int? value) {
-                          setState(() {
-                            _answers[i] = value!;
-                          });
-                        },
-                      ),
-                    );
-                  }).toList(),
-                ); // Radio
-              },
-            ),
-            TextButton.icon(
-              onPressed: (() {
-                setType(
-                    0, _testData1[0].getLabelA(), _testData1[0].getLabelB(), 1);
-                setType(
-                    1, _testData1[1].getLabelA(), _testData1[1].getLabelB(), 0);
-                setType(
-                    2, _testData1[2].getLabelA(), _testData1[2].getLabelB(), 0);
-                setType(
-                    3, _testData1[3].getLabelA(), _testData1[3].getLabelB(), 0);
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => ApplicantResult(
-                              personality_type: _testType,
-                            )));
-              }),
-              icon: const Icon(
-                Icons.app_registration,
-                size: 28,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: _testData1.length,
+                itemBuilder: (_, i) {
+                  return _radioButtonGroup(
+                    text: _testData1[i].getQuestion(),
+                    list: _testData1[i].getAnswerList().map((pair) {
+                      return ListTile(
+                        title: Text(pair.item1),
+                        leading: Radio<int>(
+                          value: pair.item2,
+                          groupValue: _answers[i],
+                          onChanged: (int? value) {
+                            setState(() {
+                              _answers[i] = value!;
+                            });
+                          },
+                        ),
+                      );
+                    }).toList(),
+                  ); // Radio
+                },
               ),
-              label: Container(
-                alignment: Alignment.center,
-                width: 150,
-                height: 35,
-                child: const Text(
-                  'Submit',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.white,
+              TextButton.icon(
+                onPressed: (() {
+                  setType(0, _testData1[0].getLabelA(),
+                      _testData1[0].getLabelB(), 1);
+                  setType(1, _testData1[1].getLabelA(),
+                      _testData1[1].getLabelB(), 0);
+                  setType(2, _testData1[2].getLabelA(),
+                      _testData1[2].getLabelB(), 0);
+                  setType(3, _testData1[3].getLabelA(),
+                      _testData1[3].getLabelB(), 0);
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ApplicantResult(
+                                personality_type: _testType,
+                                personalityData: personalityData,
+                              )));
+                }),
+                icon: const Icon(
+                  Icons.app_registration,
+                  size: 28,
+                ),
+                label: Container(
+                  alignment: Alignment.center,
+                  width: 150,
+                  height: 35,
+                  child: const Text(
+                    'Submit',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(4, 88, 125, 1),
+                    borderRadius: BorderRadius.circular(25),
                   ),
                 ),
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(4, 88, 125, 1),
-                  borderRadius: BorderRadius.circular(25),
-                ),
               ),
-            ),
-            SizedBox(
-              height: 50,
-            )
-          ],
-        ),
-      )),
+              SizedBox(
+                height: 50,
+              )
+            ],
+          ),
+       ),
+      ),
     );
   }
 }
