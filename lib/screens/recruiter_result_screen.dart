ec30e6119d826ffe27f8ab5cd0fc4bdfb4942a47
Nada Hani <66536961+nadahanii@users.noreply.github.com>
2022-07-15 16:37:40 +0200

    Update recruiter_result_screen.dart

--- lib/screens/recruiter_result_screen.dart
+++ lib/screens/recruiter_result_screen.dart
@@ -210,95 +210,95 @@ class _RecruiterResScreenState extends State<RecruiterResScreen> {
                       ),
                     ])),
 
-                itemBuilder: (_, i) => Column(
-                      crossAxisAlignment:
-                          CrossAxisAlignment.start, //AxisAlignment.center,
-                      children: [
-                        Row(
-                          //mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context)
-                                    .pushNamed('/Login_Screen');
-                              },
-                              child: Text(
-                                widget.ListOfApplicants[i].name,
-                                style: Theme.of(context).textTheme.bodyText2,
-                              ),
-                            )
-                          ],
-                        ),
-                        const SizedBox(height: 15),
-                        Row(
-                            //mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'Personality Type : ',
-                                style: Theme.of(context).textTheme.headline3,
-                              ),
-                              Text(
-                                widget.ListOfApplicants[i].personality_type,
-                                style: Theme.of(context).textTheme.headline4,
-                              ),
-                            ]),
-                        const SizedBox(height: 15),
-                        Row(
-                            children: [
-                              Text(
-                                'Kindred Spirits : ',
-                                style: Theme.of(context).textTheme.headline3,
-                              ),
-                              Text(
-                                widget.ListOfApplicants[i].kindred_spirits
-                                    .toString(),
-                                style: Theme.of(context).textTheme.headline4,
-                              ),
-                            ]),
-                        const SizedBox(height: 15),
-                        Row(
-                            //mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'Potential Complements : ',
-                                style: Theme.of(context).textTheme.headline3,
-                              ),
-                              Text(
-                                widget.ListOfApplicants[i].potential_complements
-                                    .toString(),
-                                style: Theme.of(context).textTheme.headline4,
-                              ),
-                            ]),
-                        const SizedBox(height: 15),
-                        Row(
-                            children: [
-                              Text(
-                                'Challenging Opposites : ',
-                                style: Theme.of(context).textTheme.headline3,
-                              ),
-                              Text(
-                                widget.ListOfApplicants[i].challenging_opposites
-                                    .toString(),
-                                style: Theme.of(context).textTheme.headline4,
-                              ),
-                            ]),
-                        const SizedBox(height: 15),
-                        Row(
-                            //mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'Intriguing Differences : ',
-                                style: Theme.of(context).textTheme.headline3,
-                              ),
-                              Text(
-                                widget
-                                    .ListOfApplicants[i].intriguing_differences
-                                    .toString(),
-                                style: Theme.of(context).textTheme.headline4,
-                              ),
-                            ]),
-                      ],
-                    )),
+//                 itemBuilder: (_, i) => Column(
+//                       crossAxisAlignment:
+//                           CrossAxisAlignment.start, //AxisAlignment.center,
+//                       children: [
+//                         Row(
+//                           //mainAxisAlignment: MainAxisAlignment.center,
+//                           children: [
+//                             TextButton(
+//                               onPressed: () {
+//                                 Navigator.of(context)
+//                                     .pushNamed('/Login_Screen');
+//                               },
+//                               child: Text(
+//                                 widget.ListOfApplicants[i].name,
+//                                 style: Theme.of(context).textTheme.bodyText2,
+//                               ),
+//                             )
+//                           ],
+//                         ),
+//                         const SizedBox(height: 15),
+//                         Row(
+//                             //mainAxisAlignment: MainAxisAlignment.center,
+//                             children: [
+//                               Text(
+//                                 'Personality Type : ',
+//                                 style: Theme.of(context).textTheme.headline3,
+//                               ),
+//                               Text(
+//                                 widget.ListOfApplicants[i].personality_type,
+//                                 style: Theme.of(context).textTheme.headline4,
+//                               ),
+//                             ]),
+//                         const SizedBox(height: 15),
+//                         Row(
+//                             children: [
+//                               Text(
+//                                 'Kindred Spirits : ',
+//                                 style: Theme.of(context).textTheme.headline3,
+//                               ),
+//                               Text(
+//                                 widget.ListOfApplicants[i].kindred_spirits
+//                                     .toString(),
+//                                 style: Theme.of(context).textTheme.headline4,
+//                               ),
+//                             ]),
+//                         const SizedBox(height: 15),
+//                         Row(
+//                             //mainAxisAlignment: MainAxisAlignment.center,
+//                             children: [
+//                               Text(
+//                                 'Potential Complements : ',
+//                                 style: Theme.of(context).textTheme.headline3,
+//                               ),
+//                               Text(
+//                                 widget.ListOfApplicants[i].potential_complements
+//                                     .toString(),
+//                                 style: Theme.of(context).textTheme.headline4,
+//                               ),
+//                             ]),
+//                         const SizedBox(height: 15),
+//                         Row(
+//                             children: [
+//                               Text(
+//                                 'Challenging Opposites : ',
+//                                 style: Theme.of(context).textTheme.headline3,
+//                               ),
+//                               Text(
+//                                 widget.ListOfApplicants[i].challenging_opposites
+//                                     .toString(),
+//                                 style: Theme.of(context).textTheme.headline4,
+//                               ),
+//                             ]),
+//                         const SizedBox(height: 15),
+//                         Row(
+//                             //mainAxisAlignment: MainAxisAlignment.center,
+//                             children: [
+//                               Text(
+//                                 'Intriguing Differences : ',
+//                                 style: Theme.of(context).textTheme.headline3,
+//                               ),
+//                               Text(
+//                                 widget
+//                                     .ListOfApplicants[i].intriguing_differences
+//                                     .toString(),
+//                                 style: Theme.of(context).textTheme.headline4,
+//                               ),
+//                             ]),
+//                       ],
+//                     )),
 
           ],
         ),
